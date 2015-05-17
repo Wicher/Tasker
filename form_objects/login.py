318c928ed5b20de1ba2df980df9724025a54d4d8
Szymon Szeliga <szymon.szeliga@netguru.co>
2015-05-17 15:39:13 +0200

    Add form objects for login page

---
+++ form_objects/login.py
@@ -0,0 +1,8 @@
+from flask_wtf import Form
+from wtforms import StringField, PasswordField
+from wtforms.fields.html5 import EmailField
+from wtforms.validators import DataRequired
+
+class LoginForm(Form):
+    email = EmailField('Enter your e-mail', [DataRequired()])
+    password = PasswordField('Enter your password', [DataRequired()])
--- hello.py
+++ hello.py
@@ -1,11 +1,18 @@
 import os
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 from flask.ext.script import Manager
 from flask.ext.bootstrap import Bootstrap
 from flask.ext.sqlalchemy import SQLAlchemy
+from flask.ext.auth import Auth
+from form_objects.login import LoginForm
+from pprint import pprint
+
+WTF_CSRF_SECRET_KEY = 'a random string'
+WTF_CSRF_ENABLED = False
 
 
 app = Flask(__name__)
+auth = Auth(app)
 
 manager = Manager(app)
 bootstrap = Bootstrap(app)
@@ -16,18 +23,15 @@ db = SQLAlchemy(app)
 
 
 class Users(db.Model):
-	__tablename__ = 'users'
-	id = db.Column(db.Integer, primary_key=True)
-	username = db.Column(db.String(64), unique=True, index=True)
-	
-	def __repr__(self):
-		return '<User %r>' % self.username
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), unique=True, index=True)
+
+    def __repr__(self):
+    	return '<User %r>' % self.username
 
 def init_db():
     db.create_all(app=app)
-
-
-
 
 
 @app.errorhandler(404)
@@ -37,6 +41,7 @@ def page_not_found(e):
 
 @app.errorhandler(500)
 def internal_server_error(e):
+    pprint(e)
     return render_template('500.html'), 500
 
 
@@ -45,15 +50,18 @@ def index():
 	user = Users.query.filter_by(username='Wicher').first()
 	return render_template('index.html', name=user)
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
-	return render_template('login.html')
+    form = LoginForm(request.form, csrf_enabled=False)
+    if form.validate_on_submit():
+        return redirect(url_for('index'))
+
+    return render_template('login.html', form=form)
 
 
 @app.route('/user/<name>')
 def user(name):
     return render_template('user.html', name=name)
-
 
 if __name__ == '__main__':
     manager.run()
